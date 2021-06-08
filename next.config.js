e9c8726d1d613d2bcf5f244247108074e2b0be03
sri0711 <m.sathish0711@gmail.com>
2021-06-08 10:12:03 +0530

    final touch

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
 	images: {
-		domains: ['openweathermap.org']
+		domains: ['openweathermap.org'],
+		domains: ['api.openweathermap.org']
+
 	},
 	env:{
 		apikey:process.env.API_KEY
